95188ce5990dfdae07578db0d08109143729b6ed
h-suzuki <h-suzuki@isp.co.jp>
2024-09-20 15:20:57 +0900

    feat: add source_timestamp column prefix
    Signed-off-by: h-suzuki <h-suzuki@isp.co.jp>

--- src/caret_analyze/infra/lttng/records_provider_lttng.py
+++ src/caret_analyze/infra/lttng/records_provider_lttng.py
@@ -982,6 +982,10 @@ class RecordsProviderLttng(RuntimeDataProvider):
             rename_dict[COLUMN_NAME.SOURCE_TIMESTAMP] = \
                 f'{topic_name}/{COLUMN_NAME.SOURCE_TIMESTAMP}'
 
+        if COLUMN_NAME.RMW_TAKE_TIMESTAMP in records.columns:
+            rename_dict[COLUMN_NAME.RMW_TAKE_TIMESTAMP] =\
+                f'{topic_name}/{COLUMN_NAME.RMW_TAKE_TIMESTAMP}'
+
         if COLUMN_NAME.TILDE_SUBSCRIBE_TIMESTAMP in records.columns:
             rename_dict[COLUMN_NAME.TILDE_SUBSCRIBE_TIMESTAMP] = \
                 f'{topic_name}/{COLUMN_NAME.TILDE_SUBSCRIBE_TIMESTAMP}'
@@ -1371,9 +1375,10 @@ class NodeRecordsUseLatestMessage:
             f'{self._node_path.publish_topic_name}/rclcpp_publish_timestamp',
         ]
         left_key = sub_records.columns[0]
-        if COLUMN_NAME.RMW_TAKE_TIMESTAMP in columns:
-            columns.remove(COLUMN_NAME.RMW_TAKE_TIMESTAMP)
-            left_key = COLUMN_NAME.RMW_TAKE_TIMESTAMP
+        for column in columns:
+            if column.endswith(COLUMN_NAME.RMW_TAKE_TIMESTAMP):
+                columns.remove(column)
+                left_key = column
 
         pub_sub_records = merge_sequential(
             left_records=sub_records,
